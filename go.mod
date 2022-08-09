787985bf302ff6c406fa24d568e02a62f85bd734
Chrysler Imanuel <chryslerimanuel@gmail.com>
2022-08-10 05:29:02 +0700

    p

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
 module test-deploy-go-app
 
-// +heroku goVersion go1.18.5
+// +heroku goVersion go1.18
 go 1.19
--- main.go
+++ main.go
@@ -12,21 +12,27 @@ func main() {
 	// 	fmt.Println(env)
 	// }
 
-	fmt.Println(os.Getenv("HOMEPATH"))
+	// fmt.Println(os.Getenv("HOMEPATH"))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+    port = "9000" // Default port if not specified
+	}
+
 	// //root
-	// http.HandleFunc("/", handlerHello)
+	http.HandleFunc("/", handlerHello)
 
-	// http.HandleFunc("/berhasil", handlerSuccess)
+	http.HandleFunc("/berhasil", handlerSuccess)
 
-	// http.ListenAndServe("localhost:9091", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
-// func handlerHello(w http.ResponseWriter, r *http.Request) {
-// 	var message = "Hello world!"
-// 	w.Write([]byte(message))
-// }
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "Hello world!"
+	w.Write([]byte(message))
+}
 
-// func handlerSuccess(w http.ResponseWriter, r *http.Request) {
-// 	var message = "Hore!!"
-// 	w.Write([]byte(message))
-// }
+func handlerSuccess(w http.ResponseWriter, r *http.Request) {
+	var message = "Hore!!"
+	w.Write([]byte(message))
+}
